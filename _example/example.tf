1be20bdfaf5a67b0acca60a84d2a91d33b4acc81
sureshyadav76 <thesureshyadav76@gmail.com>
2024-01-19 11:53:34 +0530

    Initial commit

---
+++ _example/example.tf
@@ -0,0 +1,11 @@
+module "labels" {
+  source = "./../"
+
+  name        = "app"
+  environment = "test"
+  label_order = ["name", "environment"]
+  attributes  = ["private"]
+  extra_tags = {
+    Application = "Demo"
+  }
+}
